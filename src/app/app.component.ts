3eaa589bf1a9a5c8315d12e33326e49e0dbc5cde
Siegfried Margin <siegfried.margin@gmail.com>
2024-04-30 15:39:55 +0200

    first input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputComponent } from './input/input.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InputComponent],
+  imports: [RouterOutlet, InputComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'to-do-app';
+ 
 }
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
 export class InputComponent {
-  submitInput(valueInput: string) {
-    console.log(`${valueInput}`);
+  firstName = new FormControl('');
+
+  submitApplication() {
+    console.log(this.firstName.value);
   }
 }
